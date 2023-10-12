217ea694727f074055dfaf164038ba73dc145408
Riddhi Parmar <riddhi.parmar@brainvire.com>
2023-10-12 12:46:38 +0530

    styled components

---
+++ src/components/styles/Nav.styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Nav = styled.nav`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 40px;
+
+  @media (max-width: ${({ theme }) => theme.mobile}) {
+    flex-direction: column;
+  }
+`;
